c68994e98ba43a6b2821e50deb73a2d617580564
Jada Grant <jagra27@morgan.edu>
2020-02-16 23:22:57 -0500

    finished app plus bonus features

---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Jada Grant on 2/16/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetContent: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
